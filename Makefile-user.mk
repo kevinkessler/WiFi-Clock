567991f75a109f418eb3052fea9864ed349f3557
kevinkessler <kkessler@kkessler.com>
2016-02-02 22:26:21 -0500

    ADC Flash

--- Makefile-user.mk
+++ Makefile-user.mk
@@ -8,7 +8,7 @@ MODULES = app
 ifeq ($(OS),Windows_NT)
 	ESP_HOME=D:/ESP8266/esp-open-sdk
 	SMING_HOME=D:/ESP8266/Sming/Sming
-	COM_PORT = COM4
+	COM_PORT = COM8
 	ESPTOOL2=D:/ESP8266/Tools/esptool2.exe
 else
 	ESP_HOME = /esptools/esp-open-sdk
@@ -37,6 +37,8 @@ RBOOT_LD_0      = rom0.ld
 TERMINAL = true
 KILL_TERM= true
 
-.PHONY: copy
+.PHONY: copy flashparam
 copy:
 	scp out/firmware/rom*.bin kevin@raspberrypi2:/var/www
+flashparam:
+	$(ESPTOOL) -p $(COM_PORT) -b $(COM_SPEED_ESPTOOL) write_flash $(flashimageoptions) 0x3fc000 esp_init_data_adc_33.bin
